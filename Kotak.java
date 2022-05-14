279fa1c96c7054135911435b9235592cbe7db474
rosaamelia137 <105532389+rosaamelia137@users.noreply.github.com>
2022-05-14 00:03:44 -0700

    Add files via upload

---
+++ Kotak.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Kotak{
+    public int panjang;
+    public int lebar;
+    public int tinggi;
+    
+    public int volume (){
+		return panjang*lebar*tinggi;
+	}
+	
+	public static void main(String [] args){
+		Kotak kotak = new Kotak();
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Masukan nilai panjang:");
+		kotak.panjang = in.nextInt();
+		
+		System.out.println("Masukan nilai lebar:");
+		kotak.lebar = in.nextInt();
+		
+		System.out.println("Masukan nilai tinggi:");
+		kotak.tinggi = in.nextInt();
+		
+		System.out.println(
+		String.format("volume kotak:%s",kotak.volume()));
+	}
+}
